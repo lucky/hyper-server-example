1dc31efd716a9d2199b4bef69e13c2d235172657
Jared Harper <jared.kuolt@gmail.com>
2022-07-16 19:55:27 -0700

    Misc updates, formatting

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,11 @@ mod tasks;
 
 use tasks::{insert_task, select_all_tasks, Task};
 
-fn do500() -> Result<Response<Body>, Error> {
-    Ok(Response::builder()
+fn do500() -> Response<Body> {
+    Response::builder()
         .status(StatusCode::INTERNAL_SERVER_ERROR)
         .body(Body::from("Internal Server Error"))
-        .unwrap())
+        .unwrap()
 }
 
 async fn post_task(
@@ -25,43 +25,43 @@ async fn post_task(
         Ok(t) => t,
         Err(e) => {
             println!("error happened: {}", e);
-            return do500();
+            return Ok(do500());
         }
     };
     match insert_task(client, t).await {
         Ok(()) => Ok(Response::new(Body::empty())),
         Err(e) => {
             println!("Error talking to database! {:?}", e);
-            Ok(Response::builder().status(500).body(Body::empty()).unwrap())
+            Ok(do500())
         }
     }
+}
+
+#[derive(Debug)]
+enum Errors {
+    Database(tokio_postgres::Error),
+    Json(serde_json::Error),
 }
 
 async fn get_tasks(
     client: Arc<Client>,
     _req: Request<Body>,
-) -> Result<Response<Body>, hyper::Error> {
-    let tasks = match select_all_tasks(client).await {
-        Ok(tasks) => tasks,
-        Err(e) => {
-            println!("Error talking to database! {:?}", e);
-            return do500();
-        }
-    };
-    let json = match serde_json::to_string(&tasks) {
-        Ok(json) => json,
-        Err(e) => {
-            println!("error happened: {}", e);
-            return do500();
-        }
-    };
+) -> Result<Response<Body>, Errors> {
+    let tasks = select_all_tasks(client).await.map_err(|e| Errors::Database(e))?;
+    let json = serde_json::to_string(&tasks).map_err(|e| Errors::Json(e))?;
     Ok(Response::new(Body::from(json)))
 }
 
 async fn serve(client: Arc<Client>, req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     match (req.method(), req.uri().path()) {
         (&Method::POST, "/tasks") => post_task(client, req).await,
-        (&Method::GET, "/tasks") => get_tasks(client, req).await,
+        (&Method::GET, "/tasks") => match get_tasks(client, req).await {
+            Ok(r) => Ok(r),
+            Err(e) => {
+                println!("Unhandled error: {:?}", e);
+                Ok(do500())
+            }
+        },
 
         // Return the 404 Not Found for other routes.
         _ => {
@@ -76,7 +76,9 @@ async fn serve(client: Arc<Client>, req: Request<Body>) -> Result<Response<Body>
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let addr = ([127, 0, 0, 1], 3030).into();
     let (client, connection) = tokio_postgres::connect(
-        std::env::var("DATABASE_URL").expect("DATABASE_URL must be set").as_str(),
+        std::env::var("DATABASE_URL")
+            .expect("DATABASE_URL must be set")
+            .as_str(),
         NoTls,
     )
     .await?;
